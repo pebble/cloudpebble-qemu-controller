db9bf8ca982afa30972d767eceebae6a23a90a5c
Katharine Berry <katharine@getpebble.com>
2015-01-26 15:47:11 -0800

    Only pick the 'binary' subprotocol if that was a valid option.

--- controller.py
+++ controller.py
@@ -122,7 +122,7 @@ def proxy_ws(emu, attr, subprotocols=[]):
     group.join()
     return ''
 
-app.app_protocol = lambda x: 'binary'
+app.app_protocol = lambda x: 'binary' if 'binary' in x else None
 
 @app.route('/qemu/<emu>/ws/phone')
 def ws_phone(emu):
