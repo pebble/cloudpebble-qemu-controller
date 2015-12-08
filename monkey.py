16156e9288fcdc6fecc2c09a36050784779e86d4
Joseph Atkins-Turkish <joeat@pebble.com>
2015-12-08 13:45:36 -0800

    Docstringed and cleaned monkey.py

--- monkey.py
+++ monkey.py
@@ -9,6 +9,12 @@ from zipfile import ZipFile
 
 class Monkey():
     def __init__(self, archive, console_port, callback_url):
+        """ Set up a Monkey test
+
+        :param archive: a file or filename which can be opened by ZipFile
+        :param console_port: The console port of the QEMU emulator to connect to
+        :param callback_url: A URL to post the results to
+        """
         self.loghash_path = settings.PEBBLE_LOGHASH_DICT
         self.runner_path = settings.PEBBLE_TEST_BIN
         if not self.loghash_path or not self.runner_path:
@@ -27,18 +33,25 @@ class Monkey():
         with ZipFile(archive) as zip:
             zip.extractall(self.tempdir)
 
-    def make_environment(self, port):
+    def make_environment(self):
+        """ :return: A copy of the current runtime environment plus the variables needed for runner.py """
         env = os.environ.copy()
         env['PEBBLE_LOGHASH_DICT'] = self.loghash_path
         env['PEBBLE_VIRTUAL_ONLY'] = '1'
-        env['PEBBLE_DEVICE'] = 'socket://localhost:{}'.format(port)
+        env['PEBBLE_DEVICE'] = 'socket://localhost:{}'.format(self.console_port)
         return env
 
     def notify_cloudpebble(self, code, log):
+        """ Notify cloudpebble of the result and log output of the test
+
+        :param code: runner.py process return code
+        :param log: runner.py STDOUT
+        """
         data = {'log': log, 'code': code, 'token': settings.LAUNCH_AUTH_HEADER}
         requests.post(self.callback_url, data=data)
 
     def wait(self):
+        """ Gevent thread. Wait for the runner to complete, then notifies CloudPebble and cleans up. """
         try:
             stdout, stderr = self.runner.communicate()
             code = self.runner.wait()
@@ -51,17 +64,18 @@ class Monkey():
         self.notify_cloudpebble(code, stdout)
 
     def run(self):
+        """ Starts the test """
         if self.is_alive():
             return
-        env = self.make_environment(self.console_port)
-        # FIXME: --ff is a workaround the fact that
+        env = self.make_environment()
+        # --ff is fail-fast, needed because runner.py doesn't return a correct failure code without it.
+        # TODO: remove --ff when bug the is fixed
         args = [self.runner_path, 'monkey', '--ff']
-        # TODO: consider the name?
-        print "RUNNING %s" % args
         self.runner = subprocess.Popen(args, cwd=self.tempdir, env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         self.thread = gevent.spawn(self.wait)
 
     def clean(self):
+        """ Delete the temporary directory containing the test files, if it exists """
         if self.tempdir:
             shutil.rmtree(self.tempdir)
             self.tempdir = None
@@ -69,6 +83,7 @@ class Monkey():
             print "Failed to delete temporary directory \"%s\"" % self.tempdir
 
     def kill(self):
+        """ Kill the test runner process and its greenlet """
         if self.runner:
             if self.runner.poll() is not None:
                 self.runner.kill()
@@ -78,5 +93,6 @@ class Monkey():
             self.thread = None
 
     def is_alive(self):
+        """ :return: True if the test runner is still alive """
         return self.runner is not None and self.thread is not None and self.runner.poll() is None
 
